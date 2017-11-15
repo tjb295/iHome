7b1a2729b32b46385ecaf3d0e7ec13ac1cc4e042
Thomas Back <tjb295@nau.edu>
2017-11-15 12:10:26 -0700

    Able to add new devices to the screen

--- app/src/main/java/net/androidbootcamp/ihome/LockScreen.java
+++ app/src/main/java/net/androidbootcamp/ihome/LockScreen.java
@@ -3,15 +3,28 @@ package net.androidbootcamp.ihome;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.view.View;
 
 public class LockScreen extends AppCompatActivity {
 
     //get linear layout so we can add child classes when adding a new device
-    LinearLayout linearLayout = (LinearLayout) findViewById(R.id.LinearLayout01);
+    int numDevices = 0;
+    String deviceName = "Whattup";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock_screen);
+    }
+
+    public void addNewDevice(View view){
+
+
+        LinearLayout layout1 = (LinearLayout) findViewById(R.id.LinearLayout01);
+        TextView device = new TextView(this);
+        device.setText(deviceName);
+        device.setId(numDevices ++);
+        layout1.addView(device);
     }
 }
