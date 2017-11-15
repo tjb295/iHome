b3b3eb54de433c781b29669f9817f334e4f7af57
Thomas Back <tjb295@nau.edu>
2017-11-15 15:29:21 -0700

    Ability to add light devices added

--- app/src/main/java/net/androidbootcamp/ihome/LightScreen.java
+++ app/src/main/java/net/androidbootcamp/ihome/LightScreen.java
@@ -2,12 +2,33 @@ package net.androidbootcamp.ihome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class LightScreen extends AppCompatActivity {
 
+    int numDevices = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_light_screen);
+    }
+    public void addNewDevice(View view){
+
+        EditText deviceInput = (EditText) findViewById(R.id.deviceInput);
+        LinearLayout layout1 = (LinearLayout) findViewById(R.id.LinearLayout01);
+        TextView device = new TextView(this);
+        device.setText(deviceInput.getText());
+        deviceInput.setText("");
+        device.setId(numDevices ++);
+        layout1.addView(device);
+
+        //Add button with device
+        SeekBar seek = new SeekBar(this);
+        layout1.addView(seek);
     }
 }
