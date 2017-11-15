96fc598abeb6c68a17bea084f7e09c499b3f44c5
Thomas Back <tjb295@nau.edu>
2017-11-15 11:53:31 -0700

    Adding the ability to add devices and enter a new textview and button for each within the linear layout in lock screens

--- app/src/main/java/net/androidbootcamp/ihome/LockScreen.java
+++ app/src/main/java/net/androidbootcamp/ihome/LockScreen.java
@@ -2,8 +2,12 @@ package net.androidbootcamp.ihome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class LockScreen extends AppCompatActivity {
+
+    //get linear layout so we can add child classes when adding a new device
+    LinearLayout linearLayout = (LinearLayout) findViewById(R.id.LinearLayout01);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
