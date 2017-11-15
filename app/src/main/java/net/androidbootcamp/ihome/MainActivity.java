758430e5a7bea7c3c4b475db9bc13a66a8283bfc
Thomas Back <tjb295@nau.edu>
2017-11-15 11:19:53 -0700

    adding link to the lock screen

--- app/src/main/java/net/androidbootcamp/ihome/MainActivity.java
+++ app/src/main/java/net/androidbootcamp/ihome/MainActivity.java
@@ -1,7 +1,9 @@
 package net.androidbootcamp.ihome;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void lockActivity(View view){
+        Intent intent = new Intent(this,LockScreen.class);
+        startActivity(intent);
     }
 }
